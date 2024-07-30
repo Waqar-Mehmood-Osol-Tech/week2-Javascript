46a57038357e732ae922e72c801ab5f9b74dffc7
Waqar-Mehmood-Osol-Tech <waqar.mahmood1@osoltech.com>
2024-07-30 16:49:53 +0500

    04-operations-in-JS

---
+++ 04-Operations.js
@@ -0,0 +1,38 @@
+//  Operations 
+let value = 3;
+let negValue = -value
+console.log(negValue); // Output: -3
+
+// console.log( 2 + 2)
+// console.log( 2 - 2)
+// console.log( 2 * 2)
+// console.log( 2**2)
+// console.log(2/3 )
+// console.log(2%3)
+
+
+let str1 = "hello"
+let str2 = "waqar"
+
+let str3 = str1 + str2
+console.log(str3)
+
+// Some Confusions
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + "3");
+// console.log(1 + 2 + "3");
+// console.log("4" + 1 + 2 + 3);
+
+// console.log(  3 + 4 * 5 % 3)
+
+
+
+console.log (+true);
+console.log (+"");
+
+
+let gameCounter = 100 ;
+gameCounter++
+console.log(gameCounter);
+
